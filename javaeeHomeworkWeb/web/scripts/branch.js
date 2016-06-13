888c5b5c8c4f00c5e66740da8d10dad789e42083
zzt <zengzt93@gmail.com>
2016-06-13 11:38:04 +0800

    add my interface add required interface

---
+++ javaeeHomeworkWeb/web/scripts/branch.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zzt on 6/13/16.
+ */
+
+function updateNum(goodsId, num) {
+
+}
+
---
+++ javaeeHomeworkWeb/web/scripts/cart.js
@@ -0,0 +1,77 @@
+/**
+ * Created by zzt on 6/13/16.
+ */
+
+var cart = Object.create(null);
+
+var UI = function () {
+    return {
+        addGoods: function (id) {
+
+        },
+        removeGoods: function (id) {
+
+        },
+        updateNum: function (id, num) {
+            
+        }
+    }
+};
+
+function addCartItem(goodsId) {
+    cart[goodsId] = newCartItem(goodsId);
+    UI.addGoods(goodsId);
+}
+
+const ADD = 1;
+const SUB = -1;
+
+function getCartItem(goodsId) {
+    return cart[goodsId];
+}
+
+function removeCartItem(goodsId) {
+    delete cart.goodsId;
+    UI.removeGoods(goodsId);
+}
+
+/**
+ * 
+ * @param goodsId
+ * @param num ADD or SUB
+ */
+function update_num(goodsId, num) {
+    var item = getCartItem(goodsId);
+    if (item.valid(num)) {
+        item.update_num(num);
+    } else {
+        removeCartItem(goodsId);
+    }
+}
+
+function newCartItem (id) {
+    return {
+        goodsId: id,
+        num: 0,
+        get_num: function () {
+            return num;
+        },
+        update_num: function (n) {
+            num += n;
+        },
+        set_num: function (n) {
+            num = n;
+        },
+        valid: function (n) {
+            return num + n > 0;
+
+        }
+    }
+}
+
+
+function clearCart() {
+    for (var id in cart) {
+        UI.removeGoods(id);
+    }
+}
