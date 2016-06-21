223b8950ea7388aba490935a080f7d980b9afa71
墨 <1533924095@qq.com>
2016-06-21 23:17:25 +0800

    new file

--- javaeeHomeworkWeb/web/scripts/pay.js
+++ javaeeHomeworkWeb/web/scripts/pay.js
@@ -1,33 +1,38 @@
 /**
  * Created by Sky on 2016/6/19.
  */
+
+var PayUI=function(){
+    return {
+        updateNum:function(dessertID,newNum,total,sumTotal){
+            var li_div=document.getElementById(dessertID);
+            if(newNum==0){
+                var ul=li_div.parentNode;
+                ul.removeChild(li_div);
+            }else{
+                var total_span=li_div.getElementsByTagName("span")[0];
+                var num_input=li_div.getElementsByTagName("input")[0];
+                num_input.value=newNum;
+                total=total.toFixed(2);
+                total_span.innerHTML=total;
+            }
+            var sumtotal_span=document.getElementById("total");
+            sumTotal=sumTotal.toFixed(2);
+            sumtotal_span.innerHTML=sumTotal;
+            change_button(sum_total);
+        },
+    }
+}
+
 function add(x){
     var button_parent= x.parentNode;   //div
     var number_input= button_parent.getElementsByClassName("number");
     var number=parseInt(number_input[0].value);
     var div_parent=button_parent.parentNode;
-    var itemTotal=div_parent.getElementsByClassName("itemTotal");
-    var span=itemTotal[0].getElementsByTagName("span");
-    var total=parseFloat(span[0].innerHTML);
-    var price=total/number;
-    var total_span=document.getElementById("total");
-    var sum_total=parseFloat(total_span.innerHTML);
-    total=total+price;
-    sum_total=sum_total+price;
-
+    var div_id=div_parent.id;
     number=number+1;
-    total=total.toFixed(2);
-    sum_total=sum_total.toFixed(2);
-    span[0].innerHTML=total;
-    number_input[0].value=number;
-    total_span.innerHTML=sum_total;
-    if(sum_total>=30){
-        var confim_button=document.getElementById("confirm_button");
-        confim_button.innerHTML="确认下单";
-        confim_button.disabled=false;
-        confim_button.style.backgroundColor="#e62625";
+    PayServer.updateNum(div_id,number);
 
-    }
 }
 
 function sub(x){
@@ -35,40 +40,48 @@ function sub(x){
     var number_input= button_parent.getElementsByClassName("number");
     var number=parseInt(number_input[0].value);
     var div_parent=button_parent.parentNode;
-    var itemTotal=div_parent.getElementsByClassName("itemTotal");
-    var span=itemTotal[0].getElementsByTagName("span");
-    var total=parseFloat(span[0].innerHTML);
-    var total_span=document.getElementById("total");
-    var sum_total=parseFloat(total_span.innerHTML);
-    var price=total/number;
-    total=total-price;
-    sum_total=sum_total-price;
+    var div_id=div_parent.id;
     number=number-1;
-    total=total.toFixed(2);
-    sum_total=sum_total.toFixed(2);
-    if(number==0){
-        var ul=div_parent.parentNode;
-        ul.removeChild(div_parent);
-    }
-    else{
-        span[0].innerHTML=total;
-        number_input[0].value=number;
-    }
-    if(sum_total<30){
-        var confim_button=document.getElementById("confirm_button");
-        var temp=30-sum_total;
-        confim_button.innerHTML="还差 "+temp+" 元起送";
-        confim_button.disabled=true;
-        confim_button.style.backgroundColor="#bbb";
+    PayServer.updateNum(div_id,number);
 
-    }
-    total_span.innerHTML=sum_total;
 
 
 }
 
-function changeNum(x,value){
-    alert(value);
+function changeNum(x){
+    var input_parent= x.parentNode;
+    var div_parent=input_parent.parentNode;
+    var dessert_id=div_parent.id;
+    var price;
+    var num= x.value;
+    if(!isNaN(num)){
+        if(num<0){
+            num=0
+        }
+    }else{
+        num=1;
+    }
+    PayServer.updateNum(dessert_id,num);
+
+
+}
+
+function change_button(v){
+    var sum_total=parseFloat(v);
+    var confim_button=document.getElementById("confirm_button");
+    if(sum_total<30){
+
+        var temp=30-sum_total;
+        confim_button.innerHTML="还差 "+temp+" 元起送";
+        confim_button.disabled=true;
+        confim_button.style.backgroundColor="#bbb";
+    }else {
+        confim_button.innerHTML="确认下单";
+        confim_button.disabled=false;
+        confim_button.style.backgroundColor="#e62625";
+    }
+
+
 }
 
 function choosePay(index){
@@ -78,3 +91,29 @@ function choosePay(index){
     lis[1].className="pay_item";
     lis[index].className="pay_item choosed";
 }
+
+function pay(){
+    toastr.success("付款成功");
+}
+
+var PayServer = function () {
+    return {
+        updateNum: function (did, newNum) {
+            $.ajax({
+                    type: 'POST',
+                    url: 'CartUpdateNumber',
+                    data: {
+                        did: did,
+                        num: newNum
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        PayUI.updateNum(did, newNum, response['lineSum'], response['newTotal']);
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    }
+                }
+            );
+        },
+    }
+}();
